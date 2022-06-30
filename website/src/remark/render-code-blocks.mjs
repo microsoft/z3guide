789bee534c1d977e4bd12f7d2f56ede93d6f6e48
Ruanqianqian (Lisa) Huang <rlisahuang@gmail.com>
2022-06-30 10:52:40 -0700

    attempt to run z3-wasm sync but still didn't get it right

--- website/src/remark/render-code-blocks.mjs
+++ website/src/remark/render-code-blocks.mjs
@@ -1,34 +1,25 @@
 
 import visit from 'unist-util-visit';
 import fs_extra_pkg from 'fs-extra';
-const { readJson, writeJson, ensureDir } = fs_extra_pkg;
+const { readJsonSync, writeJsonSync, ensureDirSync } = fs_extra_pkg;
 import { createHash } from 'crypto';
-import { init } from 'z3-solver/build/node-wrapper.js';
-import initZ3 from 'z3-solver/build/z3-built.js';
+import { init } from 'z3-solver';
 import z3pkg from 'z3-solver/package.json' assert { type: 'json' };
 
 /**
  * Turns a "```z3" code block into a code block and an output area
  */
 
-let _initializeZ3Promise
+let _initializeZ3Promise;
 async function initializeZ3() {
     if (!_initializeZ3Promise)
-        _initializeZ3Promise = (async () => {
-            const mod = await initZ3({
-                locateFile: (f) => f,
-                mainScriptUrlOrBlob: "z3-built.js",
-            });
-            const { Z3 } = await init(mod);
-            return Z3
-        })();
+        _initializeZ3Promise = await init();
     return _initializeZ3Promise;
 }
 
 
 async function getOutput(input) {
     const hashObj = createHash('sha1');
-    console.log(z3pkg.version);
 
     const hash = hashObj.update(input + z3pkg.version).digest('hex');
     // ${rise4fun engine version + z3 tool version + input}.json
@@ -36,17 +27,18 @@ async function getOutput(input) {
     const path = `./build/solutions/${hash}.json`;
     console.log(hash);
     try {
-        const data = await readJson(path);
+        const data = readJsonSync(path);
         if (data) {
             console.log(`cache hit ${hash}`)
             return data;
         }
     } catch (err) {
-    }
-    // code based on https://github.com/ViRb3/z3-wasm/blob/master/src/main.js
+        // proceed with running z3 and do nothing
+        // TODO: factor this out
 
-    /*
-    const Z3 = await initializeZ3();
+    }
+
+    const { em, Z3 } = await initializeZ3();
 
     // done on every snippet
     const cfg = Z3.mk_config();
@@ -54,26 +46,54 @@ async function getOutput(input) {
     Z3.del_config(cfg);
 
     let output = "";
-    for (const item of input.split("\n")) {
-        // TODO: specify timeout with spawn?
-        // TODO: file a bug, ask Nikolaj
-        output = output.concat(await Z3.eval_smtlib2_string(ctx, item));
+    let error = "";
+    let status = 0; // default to be success
+
+    try {
+        output = await Z3.eval_smtlib2_string(ctx, input);
+        setTimeout(() => {
+            // TODO this error won't be caught by the `catch` block
+            throw new Error('z3 times out after 500ms');
+        }, 500);
+    } catch (e) {
+        // TODO still get errors of "you cannot run the eval async..."
+        error = e.message;
+        output = "";
+
+        // when z3 times out
+        if (e.message === 'z3 times out after 500ms') {
+            console.log(`${hash}: ${error}`);
+            status = 2;
+        }
+        // when the wasm-build fails
+        else status = 1;
+    } finally {
+        try {
+            Z3.del_context(ctx);
+            em.PThread.terminateAllThreads();
+        } catch (e) {
+            // supposedly it should not happen, b/c if it fails then there is an error with our code 
+            // however, we might terminate threads when something times out, in which an abort is expected
+            // TODO: way around this?
+            console.log(`z3 aborted`);
+        }
+
     }
 
-    */
+    console.log(`z3 finished: ${hash}, ${status}`);
 
     const result = {
-        status: 0,
-        // output: output
-        output: hash
+        status: status,
+        hash: hash,
+        output: output,
+        error: error
     };
 
 
+
     // write to file
-    await writeJson(path, result);
+    writeJsonSync(path, result);
 
-
-    // default output atm
     return result;
 }
 
@@ -83,7 +103,7 @@ export default function plugin(options) {
     // console.log({ options });
     const transformer = async (ast) => {
 
-        await ensureDir('./build/solutions');
+        ensureDirSync('./build/solutions');
 
         const promises = [];
 
@@ -106,8 +126,9 @@ export default function plugin(options) {
                 return;
             }
 
+            // TODO: update `getOutput` according to Kevin's example
             promises.push(async () => {
-                console.log(`num promises: ${promises.length}; `);
+                // console.log(`num promises: ${promises.length}; `);
                 const result = await getOutput(value);
 
                 // console.log({ node, index, parent });
@@ -125,9 +146,12 @@ export default function plugin(options) {
             })
 
         });
-        console.log(`num promises: ${promises.length}`);
 
-        await Promise.all(promises.map(p => p()));
+        for (const p of promises) {
+            // need to run sync according to Kevin
+            await p();
+            console.log(`num promises: ${promises.length}`);
+        }
     };
     return transformer;
 }

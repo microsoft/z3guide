df294c343720d75cc3ee043334406844941f546a
Nikolaj Bjorner <nbjorner@microsoft.com>
2022-07-04 02:49:53 +0000

    update path to guide
    Signed-off-by: GitHub <noreply@github.com>

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs/guide/intro">
             Z3 Tutorial (content TBD)
           </Link>
         </div>

dea01a853cf00c157af18eef67a7f6cf3190fc10
Nikolaj Bjorner <nbjorner@microsoft.com>
2022-07-07 03:05:23 +0000

    path
    Signed-off-by: GitHub <noreply@github.com>

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/logic\ and\ theories/intro">
+            to="/docs/logic and theories/intro">
             Z3 Tutorial (WIP)
           </Link>
         </div>

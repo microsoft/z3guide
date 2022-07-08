75859e8d2b9b8d40e2f40ba4944cf6f3e9d90c41
Ruanqianqian (Lisa) Huang <rlisahuang@gmail.com>
2022-07-08 16:00:33 -0700

    make codeblocks editable on mouse click  (#40)
    * make codeblocks editable on mouse click TODO: figure out why content is not changing
    
    * so use-editable don't really work for our purpose; TO-try-next: react-simple-code-editor
    
    * storing code changes to state
    
    * refactor react code
    
    * share states across components
    
    * state-based method not working; trying useref
    
    * useRef preserves cursor position but doesn't work well with the Copy button
    
    * showing output by default
    
    * front-end working
    
    * cleanup

--- website/src/components/TutorialComponents/index.tsx
+++ website/src/components/TutorialComponents/index.tsx
@@ -1,41 +1,96 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
+import { Position, useEditable } from 'use-editable';
 import CodeBlock from "@theme/CodeBlock";
 
-const Output = ({ result }) => {
+function Output({ result }) {
   const success = result.status === "z3-ran";
   const emptyOutput = result.output === "";
   return (
-    <pre>
-      {success ? "" : <span style={{ color: "red" }}><b>Error: </b><br /></span>}
-      {success ?
-        emptyOutput ? "--Output is empty--" : result.output
-        : result.error}
-    </pre>
-  );
-};
-
-export default function Z3CodeBlock({ input }) {
-  const [count, setCount] = useState(0);
-  const { code, result } = input;
-  const handleClick = () => {
-    if (count > 0) {
-      setCount(count - 1);
-    } else {
-      setCount(count + 1);
-    }
-  };
-  const buttonTxt = (count > 0) ? "Click to Hide Output" : "Click to Render Output";
-  return (
     <div>
-      <button className="button button--primary" onClick={handleClick}>{buttonTxt}</button>
+      <b>Output:</b>
       <br />
-      <CodeBlock
-        language="lisp"
-        showLineNumbers
-      >
-        {code}
-      </CodeBlock>
-      {count > 0 ? <Output result={result} /> : <div />}
+      <pre>
+        {success ? "" : <span style={{ color: "red" }}><b>Error: </b><br /></span>}
+        {success ?
+          emptyOutput ? "--Output is empty--" : result.output
+          : result.error}
+      </pre>
     </div>
   );
 }
+
+function Z3Editor({ inputRef, editable, onChange }) {
+
+  const updateInput = (e) => {
+    onChange(e.target.innerText);
+  };
+
+  const codeBlock = (<CodeBlock
+    language="lisp"
+    showLineNumbers
+  >
+    {inputRef}
+  </CodeBlock>);
+
+  return (
+    <div contentEditable={editable} onInput={updateInput}>
+      {codeBlock}
+    </div>
+  );
+}
+
+function OutputToggle({ onClick }) {
+
+  return (
+    <button className="button button--primary" onClick={onClick}>
+      Run
+    </button>
+  );
+}
+
+function RunButton({ onClick }) {
+  return (
+    <button className="button button--primary" onClick={onClick}>
+      Run (with edit)
+    </button>
+  );
+}
+
+export default function Z3CodeBlock({ input }) {
+  const { code, result } = input;
+  const [newCode, updateCode] = useState(code);
+  const currCode = useRef(newCode);
+
+  const [outputRendered, setOutputRendered] = useState(false);
+
+  const [output, setOutput] = useState(result);
+
+  const onDidClickOutputToggle = () => {
+    setOutputRendered(!outputRendered);
+  };
+
+  const onDidClickRun = () => {
+    window.getSelection().removeAllRanges(); // deselect editor because cursor position gets weird
+
+    // currently only updating the output error with the new input;
+    // next goal: run z3
+    const newResult = { ...result };
+    let newOutput = `new code is: \n${newCode}`;
+    newResult.output = newOutput;
+    setOutput(newResult);
+
+    // update the data behind the editor so that CopyButton works properly after clicking Run
+    currCode.current = newCode;
+  }
+
+  return (
+    <div>
+      {outputRendered ? <div /> : <OutputToggle onClick={onDidClickOutputToggle} />}
+      {outputRendered ? <RunButton onClick={onDidClickRun} /> : <div />}
+      <Z3Editor inputRef={currCode.current} editable={outputRendered} onChange={updateCode} />
+      {outputRendered ? <Output result={output} /> : <div />}
+    </div>
+  );
+}
+
+

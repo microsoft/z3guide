433ab46d3e0d9dcbbafb65cfa935347cc962a168
Ruanqianqian (Lisa) Huang <rlisahuang@gmail.com>
2022-07-08 07:39:48 -0700

    storing code changes to state

--- website/src/components/TutorialComponents/index.tsx
+++ website/src/components/TutorialComponents/index.tsx
@@ -15,28 +15,7 @@ const Output = ({ result }) => {
   );
 };
 
-export default function Z3CodeBlock({ input }) {
-  const [count, setCount] = useState(0);
-  const { code, result } = input;
-  const handleClick = () => {
-    if (count > 0) {
-      setCount(count - 1);
-    } else {
-      setCount(count + 1);
-    }
-  };
-  const buttonTxt = (count > 0) ? "Click to Hide Output" : "Click to Render Output";
-  return (
-    <div>
-      <button className="button button--primary" onClick={handleClick}>{buttonTxt}</button>
-      <br />
-      <Z3Editor input={String(code)} />
-      {count > 0 ? <Output result={result} /> : <div />}
-    </div>
-  );
-}
-
-function Z3Editor({ input }) {
+function Z3Editor({ input, run }) {
   const [code, setCode] = useState(input);
   const editorRef = useRef(null);
   const codeArr = code.split("\n");
@@ -53,17 +32,54 @@ function Z3Editor({ input }) {
     indentation: 2
   });
 
+  const updateInput = (e) => {
+    setCode(e.target.innerText);
+    console.log(code);
+    // run.props = {disabled: false, ...run.props};
+    // setCode(e.target.value);
+  };
+
+  // run.props = { onclick: () => console.log(code), ...run.props };
   // console.log(edit.getState());
 
+  const codeBlock = (<CodeBlock
+    language="lisp"
+    showLineNumbers
+  >
+    {input}
+  </CodeBlock>);
+
   return (
-    <div ref={editorRef}>
-    <CodeBlock
-      language="lisp"
-      showLineNumbers
-    >
-      {code}
-    </CodeBlock>
+    <div contentEditable={true} onKeyUp={updateInput}>
+      {codeBlock}
     </div>
   );
 }
+
+
+export default function Z3CodeBlock({ input }) {
+  const [count, setCount] = useState(0);
+  const { code, result } = input;
+  const handleClick = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    } else {
+      setCount(count + 1);
+    }
+  };
+  const buttonTxt = (count > 0) ? "Click to Hide Output" : "Click to Render Output";
+  const btnRenderOuput = <button className="button button--primary" onClick={handleClick}>{buttonTxt}</button>;
+  const btnRun = <button className="button button--primary" disabled={true}>Run</button>
+
+  return (
+    <div>
+      {btnRenderOuput}
+      {btnRun}
+      <br />
+      <Z3Editor input={String(code)} run={btnRun} />
+      {count > 0 ? <Output result={result} /> : <div />}
+    </div>
+  );
+}
+
 

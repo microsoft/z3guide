13273ebb15e44e254d13fa5c32b1b20c67820c47
Ruanqianqian (Lisa) Huang <rlisahuang@gmail.com>
2022-07-14 20:43:57 -0700

    factored event handlers out

--- website/src/components/TutorialComponents/index.tsx
+++ website/src/components/TutorialComponents/index.tsx
@@ -63,6 +63,12 @@ function Output({ result, codeChanged }) {
 function Z3Editor(props: MyProps) {
 
   const { id, inputRef, language, showLineNumbers, editable, onChange } = props;
+  console.log(`rendered with: ${inputRef.current}`);
+
+  useEffect(() => {
+    inputRef.current = inputRef.current;
+  }, [inputRef.current]);
+
 
   const prismTheme = usePrismTheme();
   const isBrowser = useIsBrowser();
@@ -91,12 +97,12 @@ function Z3Editor(props: MyProps) {
       >
         <>
           {/* <LiveContext.Provider value={newContext}> */}
-            <LiveEditor
-              key={String(isBrowser)}
-              className={liveCodeBlockStyles.playgroundEditor}
-              code={String(inputRef.current)}
-              {...newContext}
-            />
+          <LiveEditor
+            key={String(isBrowser)}
+            className={liveCodeBlockStyles.playgroundEditor}
+            code={String(inputRef.current)}
+            {...newContext}
+          />
           {/* </LiveContext.Provider> */}
         </>
       </LiveProvider>
@@ -110,6 +116,61 @@ function Z3Editor(props: MyProps) {
 
 }
 
+// event handlers
+
+const onDidClickOutputToggle = ({ setOutputRendered, outputRendered }) => {
+  setOutputRendered(!outputRendered);
+};
+
+const onDidClickRun = ({ setRunFinished, result, currCode, setOutput, setCodeChanged }) => {
+    setRunFinished(false);
+    console.log('run clicked');
+    // TODO: only load z3 when needed
+    const newResult = { ...result };
+    let errorMsg;
+    // `z3.interrupt` -- set the cancel status of an ongoing execution, potentially with a timeout (soft? hard? we should use hard)
+    runZ3Web(currCode.code).then((res) => {
+      const result = JSON.parse(res);
+      if (result.output) {
+        const errRegex = new RegExp(/(\(error)|(unsupported)/g);
+        const hasError = result.output.match(errRegex);
+        newResult.output = hasError ? '' : result.output;
+        newResult.error = hasError ? result.output : '';
+        newResult.status = hasError ? 'z3-runtime-error' : 'z3-ran';
+      } else if (result.error) {
+        newResult.error = result.error;
+        newResult.status = 'z3-failed';
+      } else {
+        // both output and error are empty, which means we have a bug
+        errorMsg = `runZ3Web returned no output or error with input:\n${currCode.code}`
+        newResult.error = errorMsg
+        newResult.status = 'buggy-code';
+        throw new Error(errorMsg);
+      }
+    }).catch((error) => {
+      // runZ3web fails
+      errorMsg = `runZ3Web failed with input:\n${currCode.code}\n\nerror:\n${error}`;
+      newResult.error = errorMsg;
+      newResult.status = 'runZ3Web-failed';
+      throw new Error(errorMsg);
+    }).finally(() => {
+      setOutput(newResult);
+      setCodeChanged(false);
+      setRunFinished(true);
+    });
+};
+
+const onDidChangeCode = ({code, setCurrCode, outputRendered, setCodeChanged}) => {
+  setCurrCode({ code: code });
+  if (outputRendered) setCodeChanged(true);
+};
+
+const onDidClickReset = ({code, result, setCurrCode, setOutput, setCodeChanged}) => {
+  setCurrCode({ code: code });
+  setOutput(result);
+  setCodeChanged(false);
+  // codeRef.current = code;
+}
 
 export default function Z3CodeBlock({ input }) {
   const { code, result } = input;
@@ -124,79 +185,33 @@ export default function Z3CodeBlock({ input }) {
 
   const [output, setOutput] = useState(result);
 
-  const onDidClickOutputToggle = () => {
-    setOutputRendered(!outputRendered);
-  };
-
-  // bypassing server-side rendering
-  const onDidClickRun =
-    (ExecutionEnvironment.canUseDOM) ? () => {
-
-      setRunFinished(false);
-      // TODO: only load z3 when needed
-      const newResult = { ...result };
-      let errorMsg;
-      // `z3.interrupt` -- set the cancel status of an ongoing execution, potentially with a timeout (soft? hard? we should use hard)
-      runZ3Web(currCode.code).then((res) => {
-        const result = JSON.parse(res);
-        if (result.output) {
-          const errRegex = new RegExp(/(\(error)|(unsupported)/g);
-          const hasError = result.output.match(errRegex);
-          newResult.output = hasError ? '' : result.output;
-          newResult.error = hasError ? result.output : '';
-          newResult.status = hasError ? 'z3-runtime-error' : 'z3-ran';
-        } else if (result.error) {
-          newResult.error = result.error;
-          newResult.status = 'z3-failed';
-        } else {
-          // both output and error are empty, which means we have a bug
-          errorMsg = `runZ3Web returned no output or error with input:\n${currCode.code}`
-          newResult.error = errorMsg
-          newResult.status = 'buggy-code';
-          throw new Error(errorMsg);
-        }
-      }).catch((error) => {
-        // runZ3web fails
-        errorMsg = `runZ3Web failed with input:\n${currCode.code}\n\nerror:\n${error}`;
-        newResult.error = errorMsg;
-        newResult.status = 'runZ3Web-failed';
-        throw new Error(errorMsg);
-      }).finally(() => {
-        setOutput(newResult);
-        setCodeChanged(false);
-        setRunFinished(true);
-      });
-
-    } : () => { };
-
-  const onDidChangeCode = (code: string) => {
-    setCurrCode({ code: code });
-    if (outputRendered) setCodeChanged(true);
-  };
-
   const codeRef = useRef(currCode.code);
 
-  const onDidClickReset = () => {
-    setCurrCode({code: code});
-    setOutput(result);
-    setCodeChanged(false);
-    codeRef.current = currCode.code;
+  useEffect(() => {
+    if (currCode.code !== codeRef.current) {
+      codeRef.current = currCode.code;
+      console.log(`curr: ${currCode.code}`);
+      console.log(`ref: ${codeRef.current}`);
+    }
   }
-
+    , [currCode.code]);
 
   const inputNode = <>{currCode.code}</>
 
   return (
     <div>
-      {outputRendered ? <></> : <OutputToggle onClick={onDidClickOutputToggle} />}
-      {outputRendered ? <RunButton onClick={onDidClickRun} runFinished={runFinished} /> : <></>}
-      {outputRendered ? <ResetButton onClick={onDidClickReset} /> : <></>}
+      {outputRendered ? <></> : 
+      <OutputToggle onClick={() => onDidClickOutputToggle({setOutputRendered, outputRendered})} />}
+      {outputRendered ? 
+      <RunButton onClick={() => onDidClickRun({setRunFinished, result, currCode, setOutput, setCodeChanged})} runFinished={runFinished} /> 
+      : <></>}
+      {outputRendered ? <ResetButton onClick={() => onDidClickReset({code, result, setCurrCode, setOutput, setCodeChanged})} /> : <></>}
       <Z3Editor
         child={inputNode}
         inputRef={codeRef}
         id={result.hash}
         showLineNumbers={true}
-        onChange={onDidChangeCode}
+        onChange={(code) => onDidChangeCode({code, setCurrCode, outputRendered, setCodeChanged})}
         editable={outputRendered}
         language={"lisp" as Language}
       />

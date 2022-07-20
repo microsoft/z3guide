40f2afc07fdf88fa008bb19db9ea20ef17332ab5
Ruanqianqian (Lisa) Huang <rlisahuang@gmail.com>
2022-07-20 08:51:03 -0700

    Move language-specific config to a separate configuration file (#54)
    * add z3 js test file
    
    * created a separate file for language-related config used in markdown docs
    
    * factored solutionsDir out
    
    * add specified syntax highlighting to custom language without runtime
    
    * factored out timeout
    
    * experiment with loading language runtime version dynamically failed
    
    * import language runtime version at runtime
    
    * factored out status codes
    
    * factored out status codes
    
    * factored out z3 prefixes and runscript
    
    * factored website version out
    
    * attempt to make code blocks generic
    
    * update clientConfig
    
    * looks like we are unable to port language run process on the client side dynamically unless messing around with webpack config
    
    * client-side language-to-runProcess mapping
    
    * factored out status codes for client side

---
+++ website/language.config.js
@@ -0,0 +1,49 @@
+//@ts-check
+
+async function createConfig() {
+    const config = {
+        languages: [ // languages where you want to enable interactivity in editable code blocks 
+            {
+                name: 'Z3', // your language name
+                label: 'z3', // label for the language in markdown code blocks
+                highlight: 'lisp', // syntax highlighting provided by prism for the language
+                buildConfig: {
+                    version: '1',
+                    timeout: 30000, // timeout for execution of each code snippet in milliseconds during build
+                    npmPackage: 'z3-solver', // npm package name for the language runtime, if any
+                    processToExecute: './src/remark/run-z3.js',
+                    statusCodes: {
+                        success: 'z3-ran',
+                        timeout: 'z3-timed-out',
+                        runError: 'z3-failed',
+                        runtimeError: 'z3-runtime-error',
+                    },
+                },
+            },
+            {
+                name: 'Z3 JavaScript Bindings',
+                label: 'z3-js',
+                highlight: 'js',
+            },
+        ],
+        solutionsDir: './solutions',
+    }
+
+    for (const langConfig of config.languages) {
+        if (!langConfig.buildConfig) {
+            // console.log(`no config for ${langConfig.label}`);
+            continue;
+        }
+        const npmPackage = langConfig.buildConfig.npmPackage;
+        if (npmPackage) {
+            const pkgJson = `${npmPackage}/package.json`;
+            // @ts-ignore
+            const langVersion = (await import(pkgJson, { assert: { type: 'json' } })).default.version;
+            langConfig.buildConfig.langVersion = langVersion;
+        }
+    }
+
+    return config;
+}
+
+module.exports = createConfig;

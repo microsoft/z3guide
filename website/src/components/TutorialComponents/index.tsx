aed9267dfa00fe4466fef1ade76762bf5dfddc25
Ruanqianqian (Lisa) Huang <rlisahuang@gmail.com>
2022-07-27 21:17:40 +0000

    attempt to add github discussion button

--- website/src/components/TutorialComponents/index.tsx
+++ website/src/components/TutorialComponents/index.tsx
@@ -1,22 +1,29 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { LiveProvider, LiveEditor, withLive, LiveError, LivePreview, LiveContext } from 'react-live';
-import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
+import React, { useState, useRef, useEffect } from "react";
+import {
+  LiveProvider,
+  LiveEditor,
+  withLive,
+  LiveError,
+  LivePreview,
+  LiveContext,
+} from "react-live";
+import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
 import { type Props } from "@theme/CodeBlock";
-import { usePrismTheme } from '@docusaurus/theme-common';
-import useIsBrowser from '@docusaurus/useIsBrowser';
-import { Language } from 'prism-react-renderer';
-import liveCodeBlockStyles from '@docusaurus/theme-live-codeblock/src/theme/Playground/styles.module.css';
-import styles from './styles.module.css';
+import { usePrismTheme } from "@docusaurus/theme-common";
+import useIsBrowser from "@docusaurus/useIsBrowser";
+import { Language } from "prism-react-renderer";
+import liveCodeBlockStyles from "@docusaurus/theme-live-codeblock/src/theme/Playground/styles.module.css";
+import styles from "./styles.module.css";
 
 // [CONFIG HERE] custom language run process (client side) imports
-import runZ3Web from './runZ3Web';
-import runZ3JSWeb from './runZ3JSWeb';
+import runZ3Web from "./runZ3Web";
+import runZ3JSWeb from "./runZ3JSWeb";
 
 // [CONFIG HERE] language-process mapping
 const clientConfig = {
-  'z3': runZ3Web,
-  'z3-js': runZ3JSWeb,
-}
+  z3: runZ3Web,
+  "z3-js": runZ3JSWeb,
+};
 
 interface MyProps extends Props {
   readonly id: string;
@@ -27,7 +34,6 @@ interface MyProps extends Props {
 }
 
 function OutputToggle({ onClick }) {
-
   return (
     <button className="button button--primary" onClick={onClick}>
       Run
@@ -43,18 +49,40 @@ function RunButton({ onClick, runFinished }) {
   );
 }
 
+function GithubDiscussionBtn() {
+  return (
+    <a
+      className="github-button"
+      href="https://github.com/Z3Prover/z3/discussions"
+      data-color-scheme="no-preference: light; light: light; dark: dark;"
+      data-size="large"
+      aria-label="Discuss Z3Prover/z3 on GitHub"
+    >
+      Discuss
+    </a>
+  );
+}
 
 function Output({ result, codeChanged, statusCodes }) {
   const success = result.status === statusCodes.success;
   const emptyOutput = result.output === "";
   return (
     <div>
-      <b>Output{codeChanged ? ' (outdated)' : ''}:</b>
+      <b>Output{codeChanged ? " (outdated)" : ""}:</b>
       <br />
-      <pre className={codeChanged ? styles.outdated : ''}>
-        {success ? "" : <span style={{ color: "red" }}><b>Error: </b><br /></span>}
-        {success ?
-          emptyOutput ? "--Output is empty--" : result.output
+      <pre className={codeChanged ? styles.outdated : ""}>
+        {success ? (
+          ""
+        ) : (
+          <span style={{ color: "red" }}>
+            <b>Error: </b>
+            <br />
+          </span>
+        )}
+        {success
+          ? emptyOutput
+            ? "--Output is empty--"
+            : result.output
           : result.error}
       </pre>
     </div>
@@ -62,19 +90,18 @@ function Output({ result, codeChanged, statusCodes }) {
 }
 
 function CustomCodeEditor(props: MyProps) {
-
   const { id, input, language, showLineNumbers, editable, onChange } = props;
 
   const prismTheme = usePrismTheme();
   const isBrowser = useIsBrowser();
 
   const component = (
-    <div className={`${liveCodeBlockStyles.playgroundContainer} ${editable ? styles.editable : ''}`}>
-      <LiveProvider
-        code={input}
-        theme={prismTheme}
-        id={id}
-      >
+    <div
+      className={`${liveCodeBlockStyles.playgroundContainer} ${
+        editable ? styles.editable : ""
+      }`}
+    >
+      <LiveProvider code={input} theme={prismTheme} id={id}>
         <>
           <LiveEditor
             disabled={!editable}
@@ -85,19 +112,13 @@ function CustomCodeEditor(props: MyProps) {
           />
         </>
       </LiveProvider>
-    </div>);
-
-  return (
-    <>
-      {isBrowser ? component : <></>}
-    </>
+    </div>
   );
 
+  return <>{isBrowser ? component : <></>}</>;
 }
 
-
 export default function CustomCodeBlock({ input }) {
-
   const { lang, highlight, statusCodes, code, result } = input;
 
   const [currCode, setCurrCode] = useState(code);
@@ -115,58 +136,72 @@ export default function CustomCodeBlock({ input }) {
   };
 
   // bypassing server-side rendering
-  const onDidClickRun =
-    (ExecutionEnvironment.canUseDOM) ? () => {
+  const onDidClickRun = ExecutionEnvironment.canUseDOM
+    ? () => {
+        setRunFinished(false);
+        // TODO: only load z3 when needed
+        const newResult = { ...result };
+        let errorMsg;
 
-      setRunFinished(false);
-      // TODO: only load z3 when needed
-      const newResult = { ...result };
-      let errorMsg;
-
-      const runProcess = clientConfig[lang];
-      // `z3.interrupt` -- set the cancel status of an ongoing execution, potentially with a timeout (soft? hard? we should use hard)
-      runProcess(currCode).then((res) => {
-        const result = JSON.parse(res);
-        if (result.output) {
-          const errRegex = new RegExp(/(\(error)|(unsupported)|([eE]rror:)/g);
-          const hasError = result.output.match(errRegex);
-          newResult.output = hasError ? '' : result.output;
-          newResult.error = hasError ? result.output : '';
-          newResult.status = hasError ? statusCodes.runtimeError : statusCodes.success;
-        } else if (result.error) {
-          newResult.error = result.error;
-          newResult.status = statusCodes.runError;
-        } else {
-          // both output and error are empty, which means we have a bug
-          errorMsg = `${lang}-web returned no output or error with input:\n${currCode}`
-          newResult.error = errorMsg
-          newResult.status = 'buggy-code';
-          throw new Error(errorMsg);
-        }
-      }).catch((error) => {
-        // runProcess fails
-        errorMsg = `${lang}-web failed with input:\n${currCode}\n\nerror:\n${error}`;
-        newResult.error = errorMsg;
-        newResult.status = `${lang}-web-failed`;
-        throw new Error(errorMsg);
-      }).finally(() => {
-        setOutput(newResult);
-        setCodeChanged(false);
-        setRunFinished(true);
-      });
-
-    } : () => { };
+        const runProcess = clientConfig[lang];
+        // `z3.interrupt` -- set the cancel status of an ongoing execution, potentially with a timeout (soft? hard? we should use hard)
+        runProcess(currCode)
+          .then((res) => {
+            const result = JSON.parse(res);
+            if (result.output) {
+              const errRegex = new RegExp(
+                /(\(error)|(unsupported)|([eE]rror:)/g
+              );
+              const hasError = result.output.match(errRegex);
+              newResult.output = hasError ? "" : result.output;
+              newResult.error = hasError ? result.output : "";
+              newResult.status = hasError
+                ? statusCodes.runtimeError
+                : statusCodes.success;
+            } else if (result.error) {
+              newResult.error = result.error;
+              newResult.status = statusCodes.runError;
+            } else {
+              // both output and error are empty, which means we have a bug
+              errorMsg = `${lang}-web returned no output or error with input:\n${currCode}`;
+              newResult.error = errorMsg;
+              newResult.status = "buggy-code";
+              throw new Error(errorMsg);
+            }
+          })
+          .catch((error) => {
+            // runProcess fails
+            errorMsg = `${lang}-web failed with input:\n${currCode}\n\nerror:\n${error}`;
+            newResult.error = errorMsg;
+            newResult.status = `${lang}-web-failed`;
+            throw new Error(errorMsg);
+          })
+          .finally(() => {
+            setOutput(newResult);
+            setCodeChanged(false);
+            setRunFinished(true);
+          });
+      }
+    : () => {};
 
   const onDidChangeCode = (code: string) => {
     setCurrCode(code);
     if (outputRendered) setCodeChanged(true);
   };
-  const inputNode = <>{code}</>
+  const inputNode = <>{code}</>;
 
   return (
     <div>
-      {outputRendered ? <div /> : <OutputToggle onClick={onDidClickOutputToggle} />}
-      {outputRendered ? <RunButton onClick={onDidClickRun} runFinished={runFinished}/> : <div />}
+      {outputRendered ? (
+        <div />
+      ) : (
+        <OutputToggle onClick={onDidClickOutputToggle} />
+      )}
+      {outputRendered ? (
+        <RunButton onClick={onDidClickRun} runFinished={runFinished} />
+      ) : (
+        <div />
+      )}
       <CustomCodeEditor
         children={inputNode}
         input={code}
@@ -176,9 +211,15 @@ export default function CustomCodeBlock({ input }) {
         editable={outputRendered}
         language={highlight as Language}
       />
-      {outputRendered ? <Output codeChanged={codeChanged} result={output} statusCodes={statusCodes} /> : <div />}
+      {outputRendered ? (
+        <Output
+          codeChanged={codeChanged}
+          result={output}
+          statusCodes={statusCodes}
+        />
+      ) : (
+        <div />
+      )}
     </div>
   );
 }
-
-

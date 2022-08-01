e72f9e2210387a92f1e082101db8fe631972e5c6
ayanamonr <ayana@unc.edu>
2022-08-01 21:03:03 +0000

    styling changes, primary color addition

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -12,6 +12,7 @@ function HomepageHeader() {
   return (
     <div className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
+        
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>

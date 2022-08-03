6ae9cfaabbd7af9c887d89f1b5fcdb0c196f09dc
ayanamonr <ayana@unc.edu>
2022-08-03 02:32:54 +0000

    retrying design stuff

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
+// import favicon from '../static/img/favicon.ico';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
@@ -11,13 +12,14 @@ function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <div className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <div className="row" id="tryinsomthin">
+      <div className="container" id="tryinsomthin">
+        <div class="row">
+          <div class="col format">
+            <h1 className="hero__title">{siteConfig.title}</h1>
+            <p className="hero__subtitle">{siteConfig.tagline}</p>
+            <div className={styles.buttons}>
             <Link
-              className={clsx("button button--secondary button--lg", styles.mainButton)}
+              className={clsx("button button--secondary button--lg ", styles.mainButton)}
               to="/docs/logic/intro">
               Z3 SMTLIB Tutorial 
             </Link> 
@@ -33,10 +35,13 @@ function HomepageHeader() {
               to="/docs/playground/playground">
               Playground
             </Link>
+            </div>
           </div>
-
+          <div class="col">
+            <img src="/static/img/logoz3.png" alt="Z3 Logo"></img>
+          </div>
         </div>
-      </div>
+    </div>
     </div>
   );
 }

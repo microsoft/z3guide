896210b3b0cd6481f9f203592e1aca4279fdd7a4
ayanamonr <ayana@unc.edu>
2022-08-03 19:01:50 +0000

    the logo is now transparent so regardless of which background is chosen it should be fine

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -14,9 +14,9 @@ function HomepageHeader() {
     <div className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <div class="row" >
-          <div class="col format">
-            <h1 className="hero__title">{siteConfig.title}</h1>
-            <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div class="col hometextformat">
+            <h1 className="hero__title format">{siteConfig.title}</h1>
+            <p className="hero__subtitle format">{siteConfig.tagline}</p>
             <div className={styles.buttons}>
             <Link
               className={clsx("button button--secondary button--lg ", styles.mainButton)}

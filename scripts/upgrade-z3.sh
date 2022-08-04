b20fdbabbb02c8df1fe116840f11bf3a714c7406
Ruanqianqian (Lisa) Huang <rlisahuang@gmail.com>
2022-08-03 17:06:13 -0700

    test z3-upgrade script and update yarn lockfile

--- scripts/upgrade-z3.sh
+++ scripts/upgrade-z3.sh
@@ -7,20 +7,25 @@ CURR_VERSION=$(node website/get-z3-version.js);
 
 cd website;
 
-if UPGRADE_RESULTS=$(yarn upgrade z3-solver --frozen-lockfile 2>&1); then
+if UPGRADE_RESULTS=$(yarn upgrade z3-solver --latest 2>&1); then
+    echo "$UPGRADE_RESULTS";
+    echo "Z3 successfully upgraded to the latest version";
     yarn clear;
     if BUILD_RESULTS=$(yarn build 2>&1); then
-        echo "Successfully upgraded z3-solver to the latest version";
+        echo "$BUILD_RESULTS";
+        echo "Website builds successfully with the latest z3-solver";
         exit 0;
     else
         echo "Failed to build z3-solver";
         echo "$BUILD_RESULTS";
         echo "Downgrading to the previous version";
-        yarn add z3-solver@$CURR_VERSION --frozen-lockfile;
+        yarn add z3-solver@$CURR_VERSION;
     fi;
 else
     echo "Failed to upgrade z3-solver to latest";
     echo "$UPGRADE_RESULTS";
+    echo "Downgrading to the previous version";
+    yarn add z3-solver@$CURR_VERSION;
 fi;
 
 cd $CURR_DIR;

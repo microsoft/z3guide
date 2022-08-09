cdaa09fcbbeb9fab3f391c96631e283dce6252f0
Ruanqianqian (Lisa) Huang <rlisahuang@gmail.com>
2022-08-09 15:58:12 -0700

    reset button

--- website/src/components/TutorialComponents/CodeBlock.tsx
+++ website/src/components/TutorialComponents/CodeBlock.tsx
@@ -20,6 +20,7 @@ export function GithubDiscussionBtn({ repo }) {
         <button
             type="button"
             aria-label="Go to GitHub discussion"
+            title="Go to GitHub discussion"
             className={clsx(
                 'clean-btn',
                 codeBlockContentStyles.codeButton
@@ -32,7 +33,24 @@ export function GithubDiscussionBtn({ repo }) {
     );
 }
 
-
+export function ResetBtn({ resetCode, input }) {
+    return (
+        <button
+            type="button"
+            aria-label="Reset code"
+            title="Reset code"
+            className={clsx(
+                'clean-btn',
+                codeBlockContentStyles.codeButton
+            )}
+            onClick={() => resetCode(input)}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-counterclockwise" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2v1z" />
+                <path d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466z" />
+            </svg>
+        </button>
+    );
+}
 // source code of LiveEditor that allows for code editing
 // a good starting point for customizing our own code editor
 
@@ -107,6 +125,7 @@ const CodeEditor = (props) => {
             </Highlight>
             <div className={codeBlockContentStyles.buttonGroup}>
                 <CopyButton className={codeBlockContentStyles.codeButton} code={code} />
+                <ResetBtn resetCode={setCode} input={props.code} />
                 {props.githubRepo ? (
                     <GithubDiscussionBtn repo={props.githubRepo} />
                 ) : (

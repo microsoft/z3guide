fa6cbd3371e02187e06bb75ecc50cd712607c892
Ruanqianqian (Lisa) Huang <rlisahuang@gmail.com>
2022-08-18 18:01:28 -0700

    get the api to work

--- website/src/components/TutorialComponents/runZ3DuoWeb.ts
+++ website/src/components/TutorialComponents/runZ3DuoWeb.ts
@@ -19,7 +19,7 @@ declare global {
 
 
 export default async function runZ3DuoWeb(user_input: string, secret_input: string): Promise<string> {
-    
+
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     const z3 = require('z3-solver');
 
@@ -28,7 +28,7 @@ export default async function runZ3DuoWeb(user_input: string, secret_input: stri
         return window.z3Promise = z3.init();
     })();
 
-    const {  Z3: Z3Core, Context } = await z3p;
+    const { Z3: Z3Core, Context } = await z3p;
     let Z3 = Context('main');
 
     let output = '';
@@ -37,24 +37,23 @@ export default async function runZ3DuoWeb(user_input: string, secret_input: stri
     try {
         const s1 = new Z3.Solver();
         const s2 = new Z3.Solver();
-        const parse = s1.from_string(user_input);
+        s1.fromString(user_input);
+        s2.fromString(secret_input);
 
         const not_user = Z3.Not(Z3.And(s1.assertions()));
         const not_secret = Z3.Not(Z3.And(s2.assertions()));
         s2.add(not_user);
         s1.add(not_secret);
+
         const secret_not_user = await s2.check();
         const user_not_secret = await s1.check();
+
         if (secret_not_user === "sat") {
-            output = `${s2.model().sexpr()} satisfies the secret formula but not yours`;
+            output = `The model satisfies the secret formula but not yours`;
         } else if (user_not_secret === "sat") {
-            output = `${s2.model().sexpr()} satisfies the your formula but not the secret formula`;
+            output = `The model satisfies the your formula but not the secret formula`;
         } else {
-            if (secret_not_user !== "unsat" || user_not_secret !== "unsat") {
-                error = `Unexpected result from Z3: ${secret_not_user} and ${user_not_secret}`;
-            } else {
-                output = `${s2.model().sexpr()} satisfies neither your formula nor the secret formula`;
-            }
+            output = `The model satisfies neither your formula nor the secret formula`;
         }
     } catch (e) {
         // error with running z3

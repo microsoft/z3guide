18fc3c40042f8283aafcbadc1b8854755288f3f0
Ruanqianqian (Lisa) Huang <rlisahuang@gmail.com>
2022-08-19 17:48:52 -0700

    typo fix; factored out code for loading z3-solver

--- website/src/components/TutorialComponents/runZ3DuoWeb.ts
+++ website/src/components/TutorialComponents/runZ3DuoWeb.ts
@@ -1,25 +1,11 @@
-import type { evalZ3JS } from '../../eval-z3/eval-z3';
-import { type Z3HighLevel, Z3LowLevel } from 'z3-solver';
-import { init } from 'z3-solver';
-declare let { Context, setParam }: Awaited<ReturnType<typeof init>>;
-declare let Z3: ReturnType<typeof Context<'main'>>;
-
-declare global {
-    interface Window { z3Promise: Promise<Z3HighLevel & Z3LowLevel> } // use any to escape typechecking
-}
-
+import loadZ3 from './loadZ3';
 
 export default async function runZ3DuoWeb(user_input: string, secret_input: string): Promise<string> {
 
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const z3 = require('z3-solver');
-
     // init z3
-    const z3p: Promise<Z3HighLevel & Z3LowLevel> = window.z3Promise || (() => {
-        return window.z3Promise = z3.init();
-    })();
+    const z3p = loadZ3();
 
-    const { Z3: Z3Core, Context } = await z3p;
+    const { Context } = await z3p;
     let Z3 = Context('main');
 
     let output = '';
@@ -47,7 +33,7 @@ export default async function runZ3DuoWeb(user_input: string, secret_input: stri
         } else if (sat(secret_not_user)) {
             output = `${s2.model().sexpr()} satisfies the secret formula but not yours`;
         } else if (sat(user_not_secret)) {
-            output = `${s1.model().sexpr()} satisfies the your formula but not the secret formula`;
+            output = `${s1.model().sexpr()} satisfies your formula but not the secret formula`;
         } else { // both unsat
             output = `You got the right formula! Congratulations!`;
         }
